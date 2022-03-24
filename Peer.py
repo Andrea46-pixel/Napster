87a054454c43f7d2a507647df599c3d05965b611
andrea-sacchetto <andrea03stephan@gmail.com>
2022-03-24 13:08:29 +0100

    Requisiti di ogni file

--- Peer.py
+++ Peer.py
@@ -1,1 +1,15 @@
 #azioni peer specificate nel documento guido
+
+#Il singolo peer possiederà in locale:
+#il proprio session id, che indica la coppia ip-porta in ascolto
+#una lista contenente il percorso del file in condivisione e il rispettivo md5
+
+#Metodi:
+#SEND(md5)
+#CALCOLAMD5(percorso del file)
+#LOGIN(ip, porta)
+#AGGIUNGI(session id, file md5, descrizione del file)
+#DELETE(session id, md5)
+#RICERCA(session id,testo)
+#RECEIVE(md5, ip, port)
+#LOGOUT(session id)
--- Server.py
+++ Server.py
@@ -1,1 +1,9 @@
 #azioni server specificate nel documento guida
+
+#LOGIN(ip, porta)
+#CERCAPEER(session id)
+#AGGIUNGI(session id, md5, descrizione file)
+#DELETE(session id, md5)
+#RICERCA(session id, testo)
+#LOGOUT(session id)
+#REGISTRADOWN(md5, session id)
