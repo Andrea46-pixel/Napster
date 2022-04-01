30698368f775dd484b4ae7ee4bf11e2cad45f3cc
nicoccel <75250473+nicoccel@users.noreply.github.com>
2022-04-01 19:40:19 +0200

    Update Peer.py

--- Peer.py
+++ Peer.py
@@ -1,3 +1,10 @@
+import socket
+import os
+import hashlib
+import sys
+import CServer
+import Main
+
 #azioni peer specificate nel documento guido
 
 #Il singolo peer possiederà in locale:
@@ -13,3 +20,56 @@
 #RICERCA(session id,testo)
 #RECEIVE(md5, ip, port)
 #LOGOUT(session id)
+#REGISTRAZIONE
+
+class Peer:
+
+    
+
+    def __init__(self, porta, ip):
+        self.porta=porta
+        self.ip=ip
+        self.nome_utente=""
+        self.mail=""
+        self.password=""
+        self.server=socket.socket()
+        self.client=socket.socket()
+        
+    #passare mail e password
+    def __Login__(self):
+        print("")
+
+
+
+
+    def __Registrazione__(self, nome_utente, mail, password):
+        print("")
+
+
+    #passare il nome del file
+    def __Aggiungi__(self):
+        print("")
+
+    def __Delete__(self):
+        print("")
+    
+    def __Ricerca__(self):
+        print("")
+
+    def __Logout__(self):
+        self.s.close()
+
+    def __Recive__(self):
+        print("")
+
+    def __CalcoloMd5__(self):
+        print("")
+
+    def __Connessione__(self):
+
+        self.ip=socket.gethostbyname(socket.gethostname())
+
+        #connessione con il server centrale
+        self.server.connect(("25.14.181.181",80))
+        #invio messaggio di aggiornamento stato server
+        self.server.send(f"LOGI".encode)
