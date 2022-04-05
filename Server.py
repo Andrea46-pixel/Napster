3b2a279288b712eef5e970283f3ba96ef2de0a07
Andrea46-pixel <andrea03stephan@gmail.com>
2022-04-05 18:42:48 +0100

    creazione e manipolazione database

--- Server.py
+++ Server.py
@@ -10,6 +10,145 @@
 
 #porta del server 80
 
+
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+
+#connecting to the server
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+#creating database
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+create_database_query = "CREATE DATABASE school"
+create_database(connection, create_database_query)
+
+#Connecting to the Database
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+#creating a query execution function
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+
+#creating tables
+create_teacher_table = """
+CREATE TABLE teacher (
+  teacher_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  language_1 VARCHAR(3) NOT NULL,
+  language_2 VARCHAR(3),
+  dob DATE,
+  tax_id INT UNIQUE,
+  phone_no VARCHAR(20)
+  );
+ """
+
+connection = create_db_connection("localhost", "root", pw, db) # Connect to the Database
+execute_query(connection, create_teacher_table) # Execute our defined query
+
+create_course_table = """
+CREATE TABLE course (
+  course_id INT PRIMARY KEY,
+  course_name VARCHAR(40) NOT NULL,
+  language VARCHAR(3) NOT NULL,
+  level VARCHAR(2),
+  course_length_weeks INT,
+  start_date DATE,
+  in_school BOOLEAN,
+  teacher INT,
+  client INT
+);
+"""
+
+connection = create_db_connection("localhost", "root", pw, db)
+execute_query(connection, create_course_table)
+
+#populating the tables
+pop_teacher = """
+INSERT INTO teacher VALUES
+(1,  'James', 'Smith', 'ENG', NULL, '1985-04-20', 12345, '+491774553676'),
+(2, 'Stefanie',  'Martin',  'FRA', NULL,  '1970-02-17', 23456, '+491234567890'), 
+(3, 'Steve', 'Wang',  'MAN', 'ENG', '1990-11-12', 34567, '+447840921333'),
+(4, 'Friederike',  'Müller-Rossi', 'DEU', 'ITA', '1987-07-07',  45678, '+492345678901'),
+(5, 'Isobel', 'Ivanova', 'RUS', 'ENG', '1963-05-30',  56789, '+491772635467'),
+(6, 'Niamh', 'Murphy', 'ENG', 'IRI', '1995-09-08',  67890, '+491231231232');
+"""
+
+connection = create_db_connection("localhost", "root", pw, db)
+execute_query(connection, pop_teacher)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import sqlite3
 from sqlite3 import Error
 
@@ -26,9 +165,7 @@ def create_connection(path):
   
 connection = create_connection("E:\\sm_app.sqlite")
 
-#da installare
-#$ pip install mysql-connector-python
-print("prova")
+
 
 
 
