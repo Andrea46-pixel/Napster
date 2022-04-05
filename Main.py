d2981729edee52bf7e84b66290534f579c10cb7c
nicoccel <75250473+nicoccel@users.noreply.github.com>
2022-04-05 12:52:23 +0200

    sachetto leggi e rispondi

--- Main.py
+++ Main.py
@@ -2,9 +2,10 @@ import Peer
 import CServer
 import random
 
-
+#questo pezzo di codice serve per generare la porta sulla quale si mette dopo in ascolto il server del clien
+#viene salvata la porta in un file txt e poi aperta
+#se la porta non serve nel peer si potrebbe spostare questo pezzo direttamente nel server
 porta=random.randrange(49152, 65535)
-
 f=open("porta.txt", "w")
 f.write(str(porta))
 f.close()
