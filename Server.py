344b1200e8905028cc2732360556d19cfcd89cf3
Andrea46-pixel <andrea03stephan@gmail.com>
2022-04-06 09:09:47 +0100

    Manipolazione DB ok

--- Server.py
+++ Server.py
@@ -32,37 +32,19 @@ def create_server_connection(host_name, user_name, user_password, auth_plug):
 
     return connection
 
-auth_plugin = "mysql_native_password"
-hostname ="localhost"
-user_name = "**********"
-user_password = "************"
-
-create_server_connection(hostname, user_name, user_password,auth_plugin)
 
 
-
-#creating database
-def create_database(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print("Database created successfully")
-    except Error as err:
-        print(f"Error: '{err}'")
-
-
-create_database_query = "CREATE DATABASE school"
-create_database(connection, create_database_query)
 
 #Connecting to the Database
-def create_db_connection(host_name, user_name, user_password, db_name):
+def create_db_connection(host_name, user_name, user_password, db_name, auth_plug):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
             passwd=user_password,
-            database=db_name
+            database=db_name,
+            auth_plugin = auth_plug
         )
         print("MySQL Database connection successful")
     except Error as err:
@@ -70,6 +52,15 @@ def create_db_connection(host_name, user_name, user_password, db_name):
 
     return connection
 
+
+auth_plugin = "mysql_native_password"
+hostname ="localhost"
+user_name = "***********"
+user_password = "************"
+db_name = "Napster"
+
+create_server_connection(hostname, user_name, user_password,auth_plugin)
+connection = create_db_connection(hostname, user_name, user_password, db_name, auth_plugin)
 
 #creating a query execution function
 def execute_query(connection, query):
@@ -82,54 +73,24 @@ def execute_query(connection, query):
         print(f"Error: '{err}'")
 
 
-
-#creating tables
-create_teacher_table = """
-CREATE TABLE teacher (
-  teacher_id INT PRIMARY KEY,
-  first_name VARCHAR(40) NOT NULL,
-  last_name VARCHAR(40) NOT NULL,
-  language_1 VARCHAR(3) NOT NULL,
-  language_2 VARCHAR(3),
-  dob DATE,
-  tax_id INT UNIQUE,
-  phone_no VARCHAR(20)
-  );
- """
-
-connection = create_db_connection("localhost", "root", pw, db) # Connect to the Database
-execute_query(connection, create_teacher_table) # Execute our defined query
-
-create_course_table = """
-CREATE TABLE course (
-  course_id INT PRIMARY KEY,
-  course_name VARCHAR(40) NOT NULL,
-  language VARCHAR(3) NOT NULL,
-  level VARCHAR(2),
-  course_length_weeks INT,
-  start_date DATE,
-  in_school BOOLEAN,
-  teacher INT,
-  client INT
-);
-"""
-
-connection = create_db_connection("localhost", "root", pw, db)
-execute_query(connection, create_course_table)
-
 #populating the tables
-pop_teacher = """
-INSERT INTO teacher VALUES
-(1,  'James', 'Smith', 'ENG', NULL, '1985-04-20', 12345, '+491774553676'),
-(2, 'Stefanie',  'Martin',  'FRA', NULL,  '1970-02-17', 23456, '+491234567890'), 
-(3, 'Steve', 'Wang',  'MAN', 'ENG', '1990-11-12', 34567, '+447840921333'),
-(4, 'Friederike',  'Müller-Rossi', 'DEU', 'ITA', '1987-07-07',  45678, '+492345678901'),
-(5, 'Isobel', 'Ivanova', 'RUS', 'ENG', '1963-05-30',  56789, '+491772635467'),
-(6, 'Niamh', 'Murphy', 'ENG', 'IRI', '1995-09-08',  67890, '+491231231232');
+#pop_teacher = """
+#INSERT INTO UTENTE VALUES
+#('andrea', 'andrea.sacchetto@iisviolamarchesini.edu.it', 'password', '192.168.1.1', '5000', TRUE),
+#('jacopo', 'jacopo.corsatto@iisviolamarchesini.edu.it', 'password', '192.168.1.2', '5001', FALSE), 
+#('xhoni', 'xhoni.hamzaj@iisviolamarchesini.edu.it', 'password', '192.168.1.3', '5002', TRUE),
+#('nicolo', 'nicolo.risi@iisviolamarchesini.edu.it', 'password', '192.168.1.4', '5003', FALSE);
+#"""
+
+#execute_query(connection, pop_teacher)
+
+#deleting records
+delete_user = """
+DELETE FROM UTENTE
+WHERE email='192.168.1.4';
 """
 
-connection = create_db_connection("localhost", "root", pw, db)
-execute_query(connection, pop_teacher)
+execute_query(connection, delete_user)
 
 
 
@@ -158,21 +119,37 @@ execute_query(connection, pop_teacher)
 
 
 
-import sqlite3
-from sqlite3 import Error
 
-def create_connection(path):
-    connection = None
-    try:
-        connection = sqlite3.connect(path)
-        print("Connection to SQLite DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
 
-    return connection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#import sqlite3
+#from sqlite3 import Error
+
+#def create_connection(path):
+#    connection = None
+#    try:
+#        connection = sqlite3.connect(path)
+#        print("Connection to SQLite DB successful")
+#    except Error as e:
+#        print(f"The error '{e}' occurred")
+
+#    return connection
   
   
-connection = create_connection("E:\\sm_app.sqlite")
+#connection = create_connection("E:\\sm_app.sqlite")
 
 
 
