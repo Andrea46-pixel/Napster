91df1b4188ce5ada67cfe2255f1a49e41357f9ae
nicoccel <75250473+nicoccel@users.noreply.github.com>
2022-04-12 22:18:17 +0200

    Update CServer.py

--- CServer.py
+++ CServer.py
@@ -7,34 +7,53 @@ class CServer:
     def __init__(self):
 
         self.ip=""
+
         self.s=socket.socket()
-        self.stato=True
+        self.ss=socket.socket()
+
+        #self.stato=True
         f=open("porta.txt", "r").read()
-        self.porta=f
-        print(self.porta)
+        self.porta=int(f)
+        
         self.s.bind(("", int(self.porta)))
+        
+        ff=open("percorso.txt", "r").read()
+        self.percorso=ff
+
+        #self.s.bind((self.ip,self.porta))
 
 
-
+    #verificare che abbia senso
     def Ascolto(self):
-
         self.s.listen(10)
-        print("Server in ascolto...")
-        print(self.porta)
-        while self.stato==True:
+        while True:
             self.conn, self.arr=self.s.accept()
-            totstringa=self.conn.recv(4096).decode()
-            stringa=totstringa[0:4]
-
-            if stringa=="RETR":
-                print("")
-            elif stringa=="ARET":
-                print("")
-            elif stringa=="RREG":
-                print("")
+            pid=os.fork()
+            if pid==0:
+                totstringa=self.conn.recv(4096).decode()
+                stringa=totstringa[0:4]
+            
+                if stringa=="RETR":
+                    
+                    #controllare
+                    st=totstringa[5:36]
+                    f=open(self.percorso+st, os.O_RDONLY)
+                    while True:
+                        buf=os.read(f,4096)
+                        if not buf:
+                            break
+                        #controllare
+                        self.conn.send((f"ARET").encode())
+            
+                elif stringa=="ARET":
+                    print("")
+            
+                elif stringa=="RREG":
+                    print("")
 
 
 
 x=CServer()
-print("e")
-x.Ascolto()
+print("Server acceso")
+while True:
+    x.Ascolto()
