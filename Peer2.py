41a32bd1ca0fd67a70c90c7b11ec3731de9c8ff0
nicoccel <75250473+nicoccel@users.noreply.github.com>
2022-04-12 22:16:32 +0200

    Update Peer2.py

--- Peer2.py
+++ Peer2.py
@@ -1,3 +1,4 @@
+import cmd
 import random
 import os
 import socket
@@ -8,33 +9,42 @@ import sys
 #import CServer
 import subprocess
 from threading import Thread
+import signal
 
+
+#opzioni per miglioramenti
+#creare una cartella per i file md5
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
-def CalcoloPorta():
-    #questo pezzo di codice serve per generare la porta sulla quale si mette dopo in ascolto il server del clien
-    #viene salvata la porta in un file txt e poi aperta
-    #se la porta non serve nel peer si potrebbe spostare questo pezzo direttamente nel server
-    porta=random.randrange(49152, 65535)
-    f=open("porta.txt", "w")
-    f.write(str(porta))
-    f.close()
 
 class Peer:
 
+
     def __init__(self):
+        
         self.porta=""
         self.ip=""
         self.id=""
+        
         self.md5=""
         self.nomefile=""
+        
         self.ipserver=""
         self.s=socket.socket()
+        self.ss=socket.socket()
+        
+        self.percorso=""
+        #self.md5f=[]
+        #self.md5n=[]
+
+        self.portacollegamento=""
+        self.ipcollegamento=""
 
 
     def Login(self):
+        cls()
         porta=80
         self.Calcoloporta()
         #hostname = socket.gethostname()
@@ -46,8 +56,7 @@ class Peer:
             if self.ip.__len__()==15:
                 ok=True
             else:
-                print("Hai inseriti male il tuo ip")
-                
+                print("Hai inseriti male il tuo ip")   
         ok=False
         while ok==False:
             print("Inserisci l'indirizzo ip del server\nMetti gli zeri se il numero non composto da 3 cifre")
@@ -56,18 +65,15 @@ class Peer:
                 ok=True
             else:
                 print("Hai inseriti male il l'indirizzo del server")
-        
         self.s.connect((self.ipserver,porta))
-
-        self.s.send(("LOGI"+self.ip+self.porta).encode())
+        self.s.send((f"LOGI{self.ip}{self.porta}").encode())
         st=self.s.recv(4096).decode()
-        self.id=st[4:15]
+        #controllare
+        self.id=st[5:16]
     
 
     def Ricerca(self):
-        
         cls()
-        
         #sistemare
         print("Inserisci il nome del file da cercare\nCon estensione se possibile")
         self.nomefile=input()
@@ -75,17 +81,27 @@ class Peer:
         #fare in modo di vedere le iterazioni
         st=self.s.recv(4096).decode()
 
-    def Download():
+
+    def Download(self):
         #decidere se metterlo nel qui o nel server del client o un file avviato quando serve
-        print("")
+        try:
+            self.ss.connect((self.ipcollegamento,self.portacollegamento))
+            fd=os.open(self.nomefile, os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0o777)
+            while True:
+                buf=self.ss.recv(4096)
+                if not buf:
+                    break
+                os.write(fd,buf)
+            print(f"{self.nomefile} è stato scaricato correttamente")
+        except:
+            print(f"{self.nomefile} non è stato scaricato\nCi sono stati dei problemi")
+
 
     def Aggiungi(self):
-        
         cls()
-        
         print("Inserisci il nome del file che vuoi aggiungere alla condivisione\\nInserire anche l'estensione")
         #possibile miglioria
-        print("Il file deve essere presente nella cartella dove è presente questo file")
+        #print("Il file deve essere presente nella cartella dove è presente questo file")
         #^^
         self.nomefile=input()
         try:
@@ -99,30 +115,38 @@ class Peer:
         except:
             print("File non trovato")
 
-        
-        
 
     def Delete(self):
-        
         cls()
-        
         print("Quale file vuoi togliere??\nInserisci il nome")
+        nome=input()
+        if os.path.exists(f"{nome}-md5.txt")==True:
+            f=open(f"{nome}-md5.txt","r")
+            self.s.send((f"DELF{self.id}{str(f)}").encode())
+            self.s.recv(4096).decode()
+            print("file rimosso")
+        else:
+            print("File non presente\nNon è stato possibile rimuovere alcun file")
+
     
     def Logout(self):
-        
         cls()
-        
         self.s.send(("LOGO"+str(self.id)).encode())
         st=self.s.recv(4096).decode()
-        print(f"Disconnessione eseguita con successo\nAvevi condiviso {st[4:6]}")
+        print(f"Disconnessione eseguita con successo\nAvevi condiviso {st[4:6]} file")
+
 
     def Md5(self):
+        #sistemare l'appertira aggiungendo il percorso
         f=open(str(self.nomefile), "rb")
         buffer=f.read()
         self.md5=hashlib.md5(buffer).hexdigest()
-        
         #per i test
         print(self.md5)
+        #^^
+        f=open(f"{self.nomefile}-md5.txt","w")
+        f.write(str(self.md5))
+        f.close()
         time.sleep(1)
 
     
@@ -134,30 +158,58 @@ class Peer:
         #questo pezzo di codice serve per generare la porta sulla quale si mette dopo in ascolto il server del clien
         #viene salvata la porta in un file txt e poi aperta
         #se la porta non serve nel peer si potrebbe spostare questo pezzo direttamente nel server
-    
+
+
+    def Cartella(self):
+        cls()
+        #si inserisce il percorso dove si trova la cartella dei file che verranno condivisi
+        print("Inserisci il percorso della cartella dove sono presenti i file da condividere")
+        self.percorso=input()
+        f=open("percorso.txt","w")
+        f.write(str(self.percorso))
+        f.close()
 
 
 peer=Peer()
+
+if os.path.exists('filemd5')==False:
+    os.mkdir("filemd5")
+
 peer.Calcoloporta()
-#peer.Login()
-#va chiamato con un tread 
-#se eseguito così si sovrappone a questo
-#subprocess.run(["python3","CServer.py"])
-servert=Thread(subprocess.run(["python3","CServer.py"]))
-servert.start()
-uscita=False
-while uscita==False:
-    #cls()
-    print(f"Sessione {peer.id}")
-    print("Digitare:\nR per cercare un file\nA per aggiungere un file\nT per togliere un file dalla condivisione\nX per disconettersi")
-    scelta=input().upper()
-    if scelta=="R":
-        peer.Ricerca()
-    elif scelta=="A":
-        peer.Aggiungi()
-    elif scelta=="T":
-        peer.Delete()
-    elif scelta=="X":
-        peer.Logout()
-        os.close(servert)
-        uscita=True
+
+peer.Login()
+
+servert=0
+
+peer.Cartella()
+
+p=os.fork()
+if p==0:
+    uscita=False
+    while uscita==False:
+        #cls()
+        print(f"Sessione {peer.id}")
+        print(f"Percorso cartella {peer.percorso}")
+        print("Digitare:\nR per cercare un file\nA per aggiungere un file\nT per togliere un file dalla condivisione\nX per disconettersi")
+        scelta=input().upper()
+        if scelta=="R":
+            peer.Ricerca()
+        elif scelta=="A":
+            peer.Aggiungi()
+        elif scelta=="T":
+            peer.Delete()
+        elif scelta=="X":
+            peer.Logout()
+            #os.close(servert)
+            uscita=True
+            #os.killpg(os.getpid(pro.pid), signal.SIGTERM)
+            sys.exit()
+            #os._exit(os.EX_OK)
+else:
+    #va chiamato con un tread 
+    #se eseguito così si sovrappone a questo
+    #subprocess.run(["python3","CServer.py"])
+    #servert=Thread(subprocess.run(["python3","CServer.py"]))
+    #pro=subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True, preexec_fn=os.setsid)
+    servert=Thread(subprocess.run(["python3","cs2.py"]))
+    servert.start()
