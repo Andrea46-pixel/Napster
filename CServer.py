30451c0c5fadf6f29f0864e3fac9c2daa658f2c0
nicoccel <75250473+nicoccel@users.noreply.github.com>
2022-04-19 19:08:23 +0200

    Update CServer.py

--- CServer.py
+++ CServer.py
@@ -1,6 +1,7 @@
 import socket
 import os
 import sys
+from pathlib import Path
 
 class CServer:
 
@@ -8,7 +9,10 @@ class CServer:
 
         self.ip=""
 
+        #si collega al client 
         self.s=socket.socket()
+
+        #si collega al server
         self.ss=socket.socket()
 
         #self.stato=True
@@ -30,30 +34,62 @@ class CServer:
             self.conn, self.arr=self.s.accept()
             pid=os.fork()
             if pid==0:
-                totstringa=self.conn.recv(4096).decode()
-                stringa=totstringa[0:4]
+                stringa=self.conn.recv(4).decode()
             
                 if stringa=="RETR":
-                    
-                    #controllare
-                    #i cunk vanno calcolati, non si invia 
-                    st=totstringa[5:36]
-                    f=open(self.percorso+st, os.O_RDONLY)
-                    while True:
-                        buf=os.read(f,4096)
-                        if not buf:
-                            break
-                        #controllare
-                        self.conn.send((f"ARET").encode())
-            
-                elif stringa=="ARET":
-                    print("")
-            
-                elif stringa=="RREG":
-                    print("")
+                    md5=self.conn.recv(32).decode()
+                    nomefile=""
+                    try:
+                        for filename in os.listdir("filemd5"):
+                            with open(os.path.join("filemd5", filename), 'r') as f:
+                                text = f.read()
+                                if str(text)==str(md5):
+                                    nomefile=filename
+                                    break
 
+                        nomefile=nomefile[:-8]
 
+                        pachetto="AERT"
+                        if(not Path(f'{self.percorso}/{nomefile}').is_file()):
+                            self.conn.send(("AERT").encode())
+                        else:
+                            fd=os.open(f'{self.percorso}/{nomefile}', os.O_RDONLY)
+                            dimensione=os.path.getsize(rf'{self.percorso}/{nomefile}')
+                            fc=dimensione//4096
+                            resto=dimensione%4096
+                            if resto!=0:
+                                fc+=1
 
+                            tmp=""
+                            for i in range(0,6-len(str(fc))):
+                                tmp+=0
+                            tmp+=str(fc)
+                            pachetto+=tmp
+                            pachetto=str(pachetto)
+
+                            self.conn.send(pachetto.encode())
+                            pachetto=""
+
+                            for i in range(0,dimensione//4096):
+                                pachetto+="04096"
+                                pachetto+=os.read(fd,4096).decode()
+                                self.conn.send(pachetto.encode())
+                                pachetto=""
+
+                            if resto!=0:
+                                tmp=""
+                                for i in range(0,5-len(str(resto))):
+                                    tmp+=0
+                                tmp+=str(resto)
+                                pachetto+=tmp
+                                pachetto=str(pachetto)
+                                pachetto+=os.read(fd,4096).decode()
+                                self.conn.send(pachetto.encode())
+                            os.close(fd)
+                    except:
+                        self.conn.send(("AERT000000").encode())
+
+#=======================================================================================================================================
 x=CServer()
 print("Server acceso")
 while True:
